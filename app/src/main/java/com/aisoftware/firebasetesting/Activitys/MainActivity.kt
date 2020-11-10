c260576e2dc267b7eb842a4c8def0d87dd940832
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-10 16:44:56 -0600

    Creada carpeta Activitys dentro de java/ y movido archivo MainActivity.kt dentro de la misma

--- app/src/main/java/com/aisoftware/firebasetesting/Activitys/MainActivity.kt
+++ app/src/main/java/com/aisoftware/firebasetesting/Activitys/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.aisoftware.firebasetesting
+package com.aisoftware.firebasetesting.Activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aisoftware.firebasetesting.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
