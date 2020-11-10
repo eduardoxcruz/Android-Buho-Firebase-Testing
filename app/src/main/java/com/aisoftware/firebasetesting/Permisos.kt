afef884ba1b32539aa5ef3b332887daab36a70df
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-10 16:30:00 -0600

    Agregada funcion para verificar que todos los permisos necesarios para la app hayan sido concedidos

--- app/src/main/java/com/aisoftware/firebasetesting/Permisos.kt
+++ app/src/main/java/com/aisoftware/firebasetesting/Permisos.kt
@@ -1,4 +1,26 @@
 package com.aisoftware.firebasetesting
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+import com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE
+
 class Permisos {
+
+    fun VerificarQueTodosLosPermisosEstenConcedidos(contexto: Context) : Boolean{
+
+        var todosLosPermisosConcedidos : Boolean = false
+
+        if(ContextCompat.checkSelfPermission(contexto, Manifest.permission.WRITE_EXTERNAL_STORAGE) +
+           ContextCompat.checkSelfPermission(contexto, Manifest.permission.READ_EXTERNAL_STORAGE) +
+           ContextCompat.checkSelfPermission(contexto, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+
+            todosLosPermisosConcedidos = true
+        }
+
+        return todosLosPermisosConcedidos
+    }
 }
