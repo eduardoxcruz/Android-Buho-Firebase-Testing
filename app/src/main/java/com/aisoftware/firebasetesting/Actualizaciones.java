34ee3a512b157a24cb4787c5d265d0f59d12ab94
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-11 12:58:59 -0600

    Agregada funcion para consultar actualizaciones en el servidor en la clase Actualizaciones.java usando AppUpdaterUtils

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
@@ -1,4 +1,40 @@
 package com.aisoftware.firebasetesting;
 
+import android.content.Context;
+
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
+
 public class Actualizaciones {
+
+    public void ConsultarActualizacionesNuevasEnElServidor(final Context contexto){
+
+        AppUpdaterUtils consultadorDeActualizaciones = new AppUpdaterUtils(contexto)
+                .setUpdateFrom(UpdateFrom.XML).
+                        setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml")
+                .withListener(new AppUpdaterUtils.UpdateListener() {
+                    @Override
+                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+
+                        if (isUpdateAvailable)
+                        {
+
+                        }
+
+                        else
+                        {
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailed(AppUpdaterError error) {
+
+                    }
+                });
+    }
+
 }
