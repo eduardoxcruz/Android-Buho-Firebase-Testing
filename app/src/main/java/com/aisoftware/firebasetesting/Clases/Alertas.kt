a0f8c77f90c4356a47eef814acfc13f618b3a0d2
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-10 19:17:41 -0600

    Agregada funcion GenerarAlertaDeError a la clase Alertas.kt que genera un AlertDialog personalizado

--- app/src/main/java/com/aisoftware/firebasetesting/Clases/Alertas.kt
+++ app/src/main/java/com/aisoftware/firebasetesting/Clases/Alertas.kt
@@ -1,4 +1,24 @@
 package com.aisoftware.firebasetesting.Clases
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import com.aisoftware.firebasetesting.R
+
 class Alertas {
+
+    fun GenerarAlertaDeError(contexto: Context, mensajeDelDialogo : Int, accionDelClickPositivo : DialogInterface.OnClickListener, accionDelClickNegativo : DialogInterface.OnClickListener){
+
+        AlertDialog.Builder(contexto).
+            setIcon(R.drawable.error).
+            setTitle(R.string.Error).
+            setMessage(mensajeDelDialogo).
+            setCancelable(false).
+            setPositiveButton(R.string.Aceptar, accionDelClickPositivo).
+            setNegativeButton(R.string.Cancelar, accionDelClickNegativo).
+            create().
+            show()
+
+    }
+
 }
