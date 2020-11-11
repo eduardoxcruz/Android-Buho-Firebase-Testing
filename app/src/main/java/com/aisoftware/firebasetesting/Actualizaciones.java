6c40ca006089ffa77309852cda06b1d548bcb4af
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-11 13:04:06 -0600

    Agregada funcion para mostrar mensaje de que se ha encontrado actualizacion usando AppUpdater en la clase Actualizaciones.java

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
@@ -1,40 +1,63 @@
 package com.aisoftware.firebasetesting;
 
 import android.content.Context;
+import android.content.DialogInterface;
 
+import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.AppUpdaterUtils;
 import com.github.javiersantos.appupdater.enums.AppUpdaterError;
+import com.github.javiersantos.appupdater.enums.Display;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
 import com.github.javiersantos.appupdater.objects.Update;
 
 public class Actualizaciones {
 
-    public void ConsultarActualizacionesNuevasEnElServidor(final Context contexto){
+    public void ConsultarActualizacionesNuevasEnElServidor(Context contexto){
 
         AppUpdaterUtils consultadorDeActualizaciones = new AppUpdaterUtils(contexto)
-                .setUpdateFrom(UpdateFrom.XML).
-                        setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml")
-                .withListener(new AppUpdaterUtils.UpdateListener() {
-                    @Override
-                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+            .setUpdateFrom(UpdateFrom.XML)
+            .setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml")
+            .withListener(new AppUpdaterUtils.UpdateListener() {
+                @Override
+                public void onSuccess(Update update, Boolean isUpdateAvailable) {
 
-                        if (isUpdateAvailable)
-                        {
-
-                        }
-
-                        else
-                        {
-
-                        }
+                    if (isUpdateAvailable)
+                    {
 
                     }
 
-                    @Override
-                    public void onFailed(AppUpdaterError error) {
+                    else
+                    {
 
                     }
-                });
+
+                }
+
+                @Override
+                public void onFailed(AppUpdaterError error) {
+
+                }
+            });
     }
 
+
+    public void MostrarMensajeDeActualizacionDisponible(Context contexto){
+
+        AppUpdater actualizador = new AppUpdater(contexto)
+            .setDisplay(Display.DIALOG)
+            .setTitleOnUpdateAvailable(R.string.HayUnaActualizacionDisponible)
+            .setIcon(R.drawable.success)
+            .setButtonUpdate(R.string.Actualizar)
+            .setCancelable(false)
+            .setButtonDismiss(null)
+            .setButtonDoNotShowAgain(null)
+            .setUpdateFrom(UpdateFrom.XML)
+            .setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml")
+            .setButtonUpdateClickListener(new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+    }
 }
