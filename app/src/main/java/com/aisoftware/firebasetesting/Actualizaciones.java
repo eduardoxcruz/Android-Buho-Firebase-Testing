e616a2d31d4b2380dc03d61f6d7166267585d233
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-10 20:24:26 -0600

    Agregada funcion ConsultarActualizacionesNuevasEnElServidor a la clase Actualizaciones.java

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
@@ -1,4 +1,33 @@
 package com.aisoftware.firebasetesting;
 
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+
 public class Actualizaciones {
+
+    private Context contexto;
+    private AppUpdater actualizador = new AppUpdater(contexto);
+
+    private void ConsultarActualizacionesNuevasEnElServidor(){
+
+        actualizador.setDisplay(Display.DIALOG).
+                setTitleOnUpdateAvailable(R.string.HayUnaActualizacionDisponible).
+                setIcon(R.drawable.success).
+                setButtonUpdate(R.string.Actualizar).
+                setCancelable(false).
+                setButtonDismiss(null).
+                setButtonDoNotShowAgain(null).
+                setUpdateFrom(UpdateFrom.XML).
+                setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml").
+                setButtonUpdateClickListener(new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+    }
 }
