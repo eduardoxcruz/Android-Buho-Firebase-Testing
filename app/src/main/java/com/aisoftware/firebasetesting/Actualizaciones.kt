e86eebc3e86081d18833b361da480593cab66a78
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-11 13:13:36 -0600

    Convertido codigo java a codigo kotlin del archivo Actualizaciones.java

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.kt
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.kt
@@ -1,25 +1,24 @@
-package com.aisoftware.firebasetesting;
+package com.aisoftware.firebasetesting
 
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Context
+import android.content.DialogInterface
+import com.github.javiersantos.appupdater.AppUpdater
+import com.github.javiersantos.appupdater.AppUpdaterUtils
+import com.github.javiersantos.appupdater.AppUpdaterUtils.UpdateListener
+import com.github.javiersantos.appupdater.enums.AppUpdaterError
+import com.github.javiersantos.appupdater.enums.Display
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+import com.github.javiersantos.appupdater.objects.Update
 
-import com.github.javiersantos.appupdater.AppUpdater;
-import com.github.javiersantos.appupdater.AppUpdaterUtils;
-import com.github.javiersantos.appupdater.enums.AppUpdaterError;
-import com.github.javiersantos.appupdater.enums.Display;
-import com.github.javiersantos.appupdater.enums.UpdateFrom;
-import com.github.javiersantos.appupdater.objects.Update;
+class Actualizaciones {
 
-public class Actualizaciones {
+    fun ConsultarActualizacionesNuevasEnElServidor(contexto: Context?) {
 
-    public void ConsultarActualizacionesNuevasEnElServidor(Context contexto){
-
-        AppUpdaterUtils consultadorDeActualizaciones = new AppUpdaterUtils(contexto)
+        val consultadorDeActualizaciones = AppUpdaterUtils(contexto)
             .setUpdateFrom(UpdateFrom.XML)
             .setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml")
-            .withListener(new AppUpdaterUtils.UpdateListener() {
-                @Override
-                public void onSuccess(Update update, Boolean isUpdateAvailable) {
+            .withListener(object : UpdateListener {
+                override fun onSuccess(update: Update, isUpdateAvailable: Boolean) {
 
                     if (isUpdateAvailable)
                     {
@@ -30,34 +29,26 @@ public class Actualizaciones {
                     {
 
                     }
-
                 }
 
-                @Override
-                public void onFailed(AppUpdaterError error) {
+                override fun onFailed(error: AppUpdaterError) {}
+            })
 
-                }
-            });
     }
 
+    fun MostrarMensajeDeActualizacionDisponible(contexto: Context?) {
 
-    public void MostrarMensajeDeActualizacionDisponible(Context contexto){
-
-        AppUpdater actualizador = new AppUpdater(contexto)
+        val actualizador: AppUpdater = AppUpdater(contexto)
             .setDisplay(Display.DIALOG)
             .setTitleOnUpdateAvailable(R.string.HayUnaActualizacionDisponible)
             .setIcon(R.drawable.success)
             .setButtonUpdate(R.string.Actualizar)
             .setCancelable(false)
-            .setButtonDismiss(null)
-            .setButtonDoNotShowAgain(null)
+            //.setButtonDismiss(null)
+            //.setButtonDoNotShowAgain(null)
             .setUpdateFrom(UpdateFrom.XML)
             .setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml")
-            .setButtonUpdateClickListener(new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
+            .setButtonUpdateClickListener(DialogInterface.OnClickListener { dialogInterface, i -> })
 
-                }
-            });
     }
 }
