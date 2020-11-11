d812a00f723615c4d13a3dc7cd88be5c9845ba27
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-10 19:49:32 -0600

    MainActivity.kt ahora verifica que se tengan los permisos concedidos al iniciar la app. De lo contrario se solicitan los permisos y se reinicia la activity para cargar los permisos. En caso de no concederlos se cierra la app

--- app/src/main/java/com/aisoftware/firebasetesting/Activitys/MainActivity.kt
+++ app/src/main/java/com/aisoftware/firebasetesting/Activitys/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.aisoftware.firebasetesting.Activitys
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.aisoftware.firebasetesting.Clases.Alertas
+import com.aisoftware.firebasetesting.Clases.Permisos
 import com.aisoftware.firebasetesting.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val permisosDeLaApp = Permisos()
+    private val alerta = Alertas()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(permisosDeLaApp.VerificarQueTodosLosPermisosEstenConcedidos(this@MainActivity))
+
+        else
+
+            alerta.GenerarAlertaDeAviso(
+                this@MainActivity,
+                R.string.ParaQueLaAplicacionFuncioneCorrecamenteSeDebePermitirCiertosAccesosAlTelefono,
+                DialogInterface.OnClickListener(){ dialog, wich ->
+
+                    permisosDeLaApp.SolicitarPermisosRequeridos(this@MainActivity)
+
+                    Handler().postDelayed({
+                        recreate()
+                    }, 5000)
+
+                },
+                DialogInterface.OnClickListener(){ dialog, wich ->
+                    finish()
+                }
+            )
     }
 }
