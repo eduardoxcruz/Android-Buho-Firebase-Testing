3c1aa0177334ab458b1594dba42e7ff0aec3cb0f
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-11 17:41:58 -0600

    Agregue un metodo para instalar las apps perimero verificando la version de android en el que se esta ejecutando la app. Para versiones de android <= API 23 se usa la funcion intent.ACTION_VIEW de instalacion (Verificar el funcionamiento correcto de esta manera de instalacion)). Tambien agregue otro parametro requerido al metodo DescargarActualizacion ya que se requerira saber en que activity se ejecuto esta funcion para ejecutar la instalacion de la app

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
@@ -1,5 +1,6 @@
 package com.aisoftware.firebasetesting;
 
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,7 +8,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import com.github.javiersantos.appupdater.AppUpdater;
@@ -23,7 +26,7 @@ public class Actualizaciones {
     private String rutaDeDescargaDelArchivoDeActualizacion = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + nombreDelArchivoDeActualizacion;
     private Uri UriDelArchivoDeActualizacion = Uri.parse("file://" + rutaDeDescargaDelArchivoDeActualizacion);
     private File archivoDeActualizacion = new File(rutaDeDescargaDelArchivoDeActualizacion);
-    private String urlDeLaActualizacion = "https://github.com/eduardoxcruz/Publish/releases/download/1.0.0.1/app-debug.apk"; //"https://github.com/eduardoxcruz/Publish/releases/latest/download/buhoUpdate.apk";
+    private String urlDeLaActualizacion = "https://github.com/eduardoxcruz/Publish/releases/latest/download/app-debug.apk"; //"https://github.com/eduardoxcruz/Publish/releases/download/1.0.0.1/app-debug.apk";
 
     public void ConsultarActualizacionesNuevasEnElServidor(Context contexto, AppUpdaterUtils.UpdateListener accionDelListener){
 
@@ -55,7 +58,7 @@ public class Actualizaciones {
 
 
     //Fuente: https://stackoverflow.com/questions/4967669/android-install-apk-programmatically
-    public void DescargarActualizacion(final Context contexto){
+    public void DescargarActualizacion(final Context contexto, Activity activity){
 
         //TODO: First I wanted to store my update .apk file on internal storage for my app but apparently android does not allow you to open and install
         //aplication with existing package from there. So for me, alternative solution is Download directory in external storage. If there is better
@@ -73,20 +76,32 @@ public class Actualizaciones {
         DownloadManager managerDeDescarga = (DownloadManager) contexto.getSystemService(Context.DOWNLOAD_SERVICE);
         managerDeDescarga.enqueue(solicitudDeDescarga);
 
-        Toast.makeText(contexto, "Descarga iniciada. Por favor, espere...", Toast.LENGTH_LONG).show();
+        Toast.makeText(contexto, R.string.DescargaIniciada, Toast.LENGTH_LONG).show();
 
         //set BroadcastReceiver to install app when .apk is downloaded
         BroadcastReceiver AlCompletarLaDescarga = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
 
-                Toast.makeText(contexto, "¡Descarga finalizada!", Toast.LENGTH_LONG).show();
+                Toast.makeText(contexto, R.string.DescargaFinalizada, Toast.LENGTH_LONG).show();
 
                contexto.unregisterReceiver(this);
             }
         };
 
         contexto.registerReceiver(AlCompletarLaDescarga, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+    }
+
+    public void InstalarActualizacion(Activity activity){
+
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.M){
+
+            Intent instalarApp = new Intent(Intent.ACTION_VIEW);
+            instalarApp.setDataAndType(UriDelArchivoDeActualizacion, "application/vnd.android.package-archive");
+            activity.startActivity(instalarApp);
+
+        }
 
     }
 }
