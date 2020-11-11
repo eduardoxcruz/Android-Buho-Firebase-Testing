2670cc7a8f14403fccc305ff504d27304a0aac87
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-10 20:36:30 -0600

    Agregados getters y setters de hayActualizacionesDisponibles. Modificada la funcion ConsultarActualizacionesNuevasEnElServidor, ahora se usa AppUdaterUtils para consultar si hay actualizaciones

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
@@ -4,32 +4,51 @@ import android.content.Context;
 import android.content.DialogInterface;
 
 import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
 import com.github.javiersantos.appupdater.enums.Display;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
 
 public class Actualizaciones {
 
     private Context contexto;
     private AppUpdater actualizador = new AppUpdater(contexto);
+    private AppUpdaterUtils consultadorDeActualizaciones= new AppUpdaterUtils(contexto);
+    private boolean hayActualizacionDisponible;
+
+    public boolean getHayActualizacionDisponible() {
+        return hayActualizacionDisponible;
+    }
+
+    public void setHayActualizacionDisponible(boolean value) {
+        this.hayActualizacionDisponible = value;
+    }
 
     public Actualizaciones(Context contexto){
         this.contexto = contexto;
+        this.hayActualizacionDisponible = true;
     }
 
     private void ConsultarActualizacionesNuevasEnElServidor(){
 
-        actualizador.setDisplay(Display.DIALOG).
-                setTitleOnUpdateAvailable(R.string.HayUnaActualizacionDisponible).
-                setIcon(R.drawable.success).
-                setButtonUpdate(R.string.Actualizar).
-                setCancelable(false).
-                setButtonDismiss(null).
-                setButtonDoNotShowAgain(null).
-                setUpdateFrom(UpdateFrom.XML).
+        consultadorDeActualizaciones.setUpdateFrom(UpdateFrom.XML).
                 setUpdateXML("https://algoritmosinteligentes.000webhostapp.com/VersionesTQ/androidver.xml").
-                setButtonUpdateClickListener(new DialogInterface.OnClickListener() {
+                withListener(new AppUpdaterUtils.UpdateListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+
+                        if(isUpdateAvailable == true){
+
+                        }
+
+                        else{
+                            setHayActualizacionDisponible(false);
+                        }
+                    }
+
+                    @Override
+                    public void onFailed(AppUpdaterError error) {
 
                     }
                 });
