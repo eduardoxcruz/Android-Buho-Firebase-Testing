d7f7d610d5b29b53cf74b60b58b18c021c7652d3
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-11 13:59:08 -0600

    MainActivity.kt busca actualizaciones en el metodo OnCreate despues de verificar que se tienen los permisos necesarios para la app. En caso de tener un error con las actualizaciones se muestra un AlertDialog con un mensaje de error en el servidor. En caso de encontrar actualizaciones se le muestra al usuario un mensaje de aviso que hay actualizacion

--- app/src/main/java/com/aisoftware/firebasetesting/Activitys/MainActivity.kt
+++ app/src/main/java/com/aisoftware/firebasetesting/Activitys/MainActivity.kt
@@ -2,21 +2,59 @@ package com.aisoftware.firebasetesting.Activitys
 
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.aisoftware.firebasetesting.Actualizaciones
 import com.aisoftware.firebasetesting.Clases.Alertas
 import com.aisoftware.firebasetesting.Clases.Permisos
 import com.aisoftware.firebasetesting.R
+import com.github.javiersantos.appupdater.AppUpdaterUtils
+import com.github.javiersantos.appupdater.enums.AppUpdaterError
+import com.github.javiersantos.appupdater.objects.Update
 
 class MainActivity : AppCompatActivity() {
 
     private val permisosDeLaApp = Permisos()
     private val alerta = Alertas()
+    private val actualizadorDeLaApp = Actualizaciones()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if(permisosDeLaApp.VerificarQueTodosLosPermisosEstenConcedidos(this@MainActivity))
+        {
+            actualizadorDeLaApp.ConsultarActualizacionesNuevasEnElServidor(this@MainActivity, object :
+                AppUpdaterUtils.UpdateListener {
+                override fun onSuccess(update: Update, isUpdateAvailable: Boolean) {
+
+                    if (isUpdateAvailable)
+                    {
+                        actualizadorDeLaApp.MostrarMensajeDeActualizacionDisponible(this@MainActivity)
+                    }
+
+                    else
+                    {
+
+                    }
+                }
+
+                override fun onFailed(error: AppUpdaterError)
+                {
+
+                    alerta.GenerarAlertaDeError(
+                        this@MainActivity,
+                        R.string.HuboUnErrorAlConectarConElServidorDeActualizaciones,
+                        { dialog, wich ->
+                            dialog.dismiss()
+                        },
+                        { dialog, wich ->
+                            dialog.dismiss()
+                        }
+                    )
+                }
+            })
+        }
 
         else
         {
