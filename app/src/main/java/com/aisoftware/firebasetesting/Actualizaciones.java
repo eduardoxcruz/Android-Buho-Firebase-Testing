3ee91074278aa445d692fd80493f6bbfe73f248e
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-11-11 15:13:38 -0600

    Agregadas propiedades del archivo de actualizacion a la clase Actualizaciones.java (por defecto sera la carpeta de descargas de la memoria interna)

--- app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Actualizaciones.java
@@ -2,13 +2,22 @@ package com.aisoftware.firebasetesting;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Environment;
 
 import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.AppUpdaterUtils;
 import com.github.javiersantos.appupdater.enums.Display;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
 
+import java.io.File;
+
 public class Actualizaciones {
+
+    private String nombreDelArchivoDeActualizacion = "buhoUpdate.apk";
+    private String rutaDeDescargaDelArchivoDeActualizacion = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + nombreDelArchivoDeActualizacion;
+    private Uri UriDelArchivoDeActualizacion = Uri.parse("file://" + rutaDeDescargaDelArchivoDeActualizacion);
+    private File archivoDeActualizacion = new File(rutaDeDescargaDelArchivoDeActualizacion);
 
     public void ConsultarActualizacionesNuevasEnElServidor(Context contexto, AppUpdaterUtils.UpdateListener accionDelListener){
 
