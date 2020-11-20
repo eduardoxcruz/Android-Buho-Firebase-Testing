526be4865c7f927df5c45912b9a3edf124c419b2
Eduardo <eduardoxcruz2000@hotmail.com>
2020-11-19 18:42:04 -0600

    Agregue las propiedades necesarias para la clase y su inizializacion desde el constructor por defecto

--- app/src/main/java/com/aisoftware/firebasetesting/Clases/Turno.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Clases/Turno.java
@@ -1,8 +1,26 @@
 package com.aisoftware.firebasetesting.Clases;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
 public class Turno {
 
+    private FirebaseDatabase database;
+    private DatabaseReference refDatabase;
+    private Alertas alerta;
+    private Calendar calendario;
+    private int horaActual;
+    private String turnoActual;
+
     public Turno(){
+
+        this.database = FirebaseDatabase.getInstance();
+        this.refDatabase = database.getReference();
+        this.alerta = new Alertas();
+        this.calendario = Calendar.getInstance();
+        this.horaActual = calendario.get(Calendar.HOUR_OF_DAY);
 
     }
 
