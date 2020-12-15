2ee756398e0392e00e7495f9c97d1ba41fd20359
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2020-12-15 11:04:43 -0600

    Agregue propiedades de Firebase Database y constructor con parametros por defecto en FirebaseRealtimeDatabase

--- app/src/main/java/com/aisoftware/firebasetesting/Databases/FirebaseRealtimeDatabase.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Databases/FirebaseRealtimeDatabase.java
@@ -1,4 +1,18 @@
 package com.aisoftware.firebasetesting.Databases;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FirebaseRealtimeDatabase {
+
+    private FirebaseDatabase database;
+    private DatabaseReference DatabaseReference;
+
+    public FirebaseRealtimeDatabase(){
+
+        this.database = FirebaseDatabase.getInstance();
+        this.DatabaseReference = database.getReference();
+
+    }
+
 }
