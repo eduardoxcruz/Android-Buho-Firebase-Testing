da96b7a37946b7e6f542fe98c03ca50de3be4917
Eduardo <eduardoxcruz2000@hotmail.com>
2020-12-16 10:38:27 -0600

    Agregue la propiedad notificationBuilder y su inicializacion en el constructor de la clase.

--- app/src/main/java/com/aisoftware/firebasetesting/Clases/Notificaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Clases/Notificaciones.java
@@ -2,13 +2,17 @@ package com.aisoftware.firebasetesting.Clases;
 
 import android.content.Context;
 
+import androidx.core.app.NotificationCompat;
+
 public class Notificaciones {
 
     private Context contexto;
+    private NotificationCompat.Builder notificationBuilder;
 
     public Notificaciones(Context Contexto){
 
         this.contexto = Contexto;
+        this.notificationBuilder = new NotificationCompat.Builder(contexto, CHANNEL_ID);
 
     }
 
