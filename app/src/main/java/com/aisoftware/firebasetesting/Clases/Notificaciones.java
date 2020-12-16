985ae68efd296b3bf107109ba82193d693400c5a
Eduardo <eduardoxcruz2000@hotmail.com>
2020-12-16 10:34:16 -0600

    Agregue la propiedad Context y su contstructor de la clase Notificaciones.java

--- app/src/main/java/com/aisoftware/firebasetesting/Clases/Notificaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Clases/Notificaciones.java
@@ -1,4 +1,15 @@
 package com.aisoftware.firebasetesting.Clases;
 
+import android.content.Context;
+
 public class Notificaciones {
+
+    private Context contexto;
+
+    public Notificaciones(Context Contexto){
+
+        this.contexto = Contexto;
+
+    }
+
 }
