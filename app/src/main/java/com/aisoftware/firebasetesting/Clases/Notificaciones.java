9a0934998a2b5729755a129fc9607a02df076bd1
Eduardo <eduardoxcruz2000@hotmail.com>
2020-12-18 11:56:26 -0600

    Agregue la funcion para crear canales de notificacion en la clase Notificaciones.java

--- app/src/main/java/com/aisoftware/firebasetesting/Clases/Notificaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Clases/Notificaciones.java
@@ -1,6 +1,9 @@
 package com.aisoftware.firebasetesting.Clases;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
@@ -12,7 +15,25 @@ public class Notificaciones {
     public Notificaciones(Context Contexto){
 
         this.contexto = Contexto;
-        this.notificationBuilder = new NotificationCompat.Builder(contexto, CHANNEL_ID);
+
+    }
+
+    public void CrearCanalDeNotificacion(int id, int nombre_del_canal, int descripcion_del_canal, int importancia){
+
+        String ID = contexto.getString(id);
+        CharSequence nombreDelCanal = contexto.getString(nombre_del_canal);
+        String descripcionDelCanal = contexto.getString(descripcion_del_canal);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+
+            NotificationChannel canalDeNotificacion = new NotificationChannel(ID, nombreDelCanal, importancia);
+            canalDeNotificacion.setDescription(descripcionDelCanal);
+
+            NotificationManager managerDeNotificacion = contexto.getSystemService(NotificationManager.class);
+            managerDeNotificacion.createNotificationChannel(canalDeNotificacion);
+
+        }
 
     }
 
