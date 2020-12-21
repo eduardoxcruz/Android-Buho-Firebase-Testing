1013ee501d9c75d59abed75608f9a08c0406ece6
Eduardo <eduardoxcruz2000@hotmail.com>
2020-12-21 15:50:46 -0600

    Agregue el constructor por defecto de la clase Internet

--- app/src/main/java/com/aisoftware/firebasetesting/Clases/Internet.java
+++ app/src/main/java/com/aisoftware/firebasetesting/Clases/Internet.java
@@ -1,4 +1,15 @@
 package com.aisoftware.firebasetesting.Clases;
 
+import android.content.Context;
+
 public class Internet {
+
+    private Context contexto;
+
+    public Internet(Context contexto){
+
+        this.contexto = contexto;
+
+    }
+
 }
