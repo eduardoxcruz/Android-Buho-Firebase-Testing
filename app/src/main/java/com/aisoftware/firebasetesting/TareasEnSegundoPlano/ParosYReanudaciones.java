0696aa09c6f8ee154e28223005dba66872040404
Eduardo <eduardoxcruz2000@hotmail.com>
2020-12-21 20:53:18 -0600

    Revert "La clase ParosYReanudaciones.java hereda de Worker"
    This reverts commit f6689c22

--- app/src/main/java/com/aisoftware/firebasetesting/TareasEnSegundoPlano/ParosYReanudaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/TareasEnSegundoPlano/ParosYReanudaciones.java
@@ -1,21 +1,4 @@
 package com.aisoftware.firebasetesting.TareasEnSegundoPlano;
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-import androidx.work.Worker;
-import androidx.work.WorkerParameters;
-
-public class ParosYReanudaciones extends Worker {
-
-    public ParosYReanudaciones(@NonNull Context context, @NonNull WorkerParameters workerParams) {
-        super(context, workerParams);
-    }
-
-    @NonNull
-    @Override
-    public Result doWork() {
-        return null;
-    }
-
+public class ParosYReanudaciones {
 }
