f6689c222707a104b430f43c29d92a92701dede4
Eduardo <eduardoxcruz2000@hotmail.com>
2020-12-21 18:42:14 -0600

    La clase ParosYReanudaciones.java hereda de Worker

--- app/src/main/java/com/aisoftware/firebasetesting/TareasEnSegundoPlano/ParosYReanudaciones.java
+++ app/src/main/java/com/aisoftware/firebasetesting/TareasEnSegundoPlano/ParosYReanudaciones.java
@@ -1,4 +1,21 @@
 package com.aisoftware.firebasetesting.TareasEnSegundoPlano;
 
-public class ParosYReanudaciones {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class ParosYReanudaciones extends Worker {
+
+    public ParosYReanudaciones(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
+
 }
