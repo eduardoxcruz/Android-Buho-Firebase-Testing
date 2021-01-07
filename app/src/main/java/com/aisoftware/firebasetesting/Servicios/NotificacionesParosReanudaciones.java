57cba30995facb44d603667bb3865a321923a2d2
Eduardo <eduardoxcruz2000@hotmail.com>
2021-01-07 16:01:53 -0600

    Agregue una nueva clase que hereda de FirebaseMessagingService para notificar por paros y reanudaciones

---
+++ app/src/main/java/com/aisoftware/firebasetesting/Servicios/NotificacionesParosReanudaciones.java
@@ -0,0 +1,6 @@
+package com.aisoftware.firebasetesting.Servicios;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class NotificacionesParosReanudaciones extends FirebaseMessagingService {
+}
