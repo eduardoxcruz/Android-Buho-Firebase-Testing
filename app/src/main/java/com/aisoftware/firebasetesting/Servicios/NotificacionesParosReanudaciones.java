b789b89d0d7867a8223be8e0ef65f645f9939cab
Eduardo <eduardoxcruz2000@hotmail.com>
2021-01-13 15:02:38 -0600

    Elimine los archivos anteriores que servian para como servicios en segundo plano y el primer archivod de FCM

--- app/src/main/java/com/aisoftware/firebasetesting/Servicios/NotificacionesParosReanudaciones.java
+++
@@ -1,6 +0,0 @@
-package com.aisoftware.firebasetesting.Servicios;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class NotificacionesParosReanudaciones extends FirebaseMessagingService {
-}
