122b89cf806646240cc3dee9ad8b6c5f97b861e6
Eduardo <eduardoxcruz2000@hotmail.com>
2021-01-13 14:57:04 -0600

    Agregue un nuevo archivo que sera el sustituto de la anterior clase para notificaciones push de FCM

---
+++ app/src/main/java/com/aisoftware/firebasetesting/Servicios/FCM_PushNotifications.java
@@ -0,0 +1,6 @@
+package com.aisoftware.firebasetesting.Servicios;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCM_PushNotifications extends FirebaseMessagingService {
+}
