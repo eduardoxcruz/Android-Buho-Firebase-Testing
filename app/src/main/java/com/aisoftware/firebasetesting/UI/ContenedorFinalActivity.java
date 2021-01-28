392b9bd74a2d32c579da2128c4b9e34885f9f482
Eduardo <eduardoxcruz2000@hotmail.com>
2021-01-28 13:09:42 -0600

    Agregue una nueva activity que sera la que contenga todas las vistas y fragments de la app

---
+++ app/src/main/java/com/aisoftware/firebasetesting/UI/ContenedorFinalActivity.java
@@ -0,0 +1,16 @@
+package com.aisoftware.firebasetesting.UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aisoftware.firebasetesting.R;
+
+public class ContenedorFinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contenedor_final);
+    }
+}
